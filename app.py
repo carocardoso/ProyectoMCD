ed0d42f5ea2c8201465a0e53c5d6e2f8b5b9254f
carocardoso <alejccardoso@gmail.com>
2026-01-12 19:18:24 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+
+import streamlit as st
+import pandas as pd
+# from app.datos_gral import mostrar_datos_gral
+# from app.dashboard_proceso import mostrar_procesados
+# from app.info_proyecto import mostrar_info_proyecto
+# from app.carreras_sel import mostrar_carreras_sel
+
+from streamlit_option_menu import option_menu
+import os
+
+# --- Configuración general ---
+st.set_page_config(
+    page_title="Proyecto de Tesis Maestría en Ciencia de Datos",
+    page_icon="📊",
+    layout="wide"
+)
+
+with st.sidebar:
+    seccion=option_menu(
+        menu_title="",
+        options=["Información del Proyecto", "Estadísticas", "Carreras","Tópicos"],
+        icons=["info-circle", "funnel", "bar-chart", "graph-up-arrow"])   
+
+if seccion == "Información del Proyecto":
+    #mostrar_info_proyecto()
+    st.write('info proy')
+
+elif seccion == "Estadísticas":
+     #mostrar_datos_gral() #mostrar_datos2()
+    st.write('estadi')
+    
+elif seccion == "Carreras":
+    #mostrar_carreras_sel()
+    st.write('carreras')
+    
+elif seccion == "Tópicos":
+    #mostrar_procesados()
+    st.write('topico')
+    
