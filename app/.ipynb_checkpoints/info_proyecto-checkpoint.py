e6a9134e70e157b44f88bfef6ed607aea062bca5
carocardoso <alejccardoso@gmail.com>
2026-01-12 20:26:34 -0300

    .

---
+++ app/.ipynb_checkpoints/info_proyecto-checkpoint.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+def mostrar_info_proyecto():
+    st.title("ℹ️ Información del Proyecto")
+    st.markdown("""
+    Esta aplicación muestra el desarrollo del proyecto de análisis de trabajos finales de carreras de grado de la Universidad Católica de Salta.
+    Este proyecto forma parte de la tesis de maestría y tiene como propósito analizar tendencias en los trabajos finales de distintas carreras universitarias.
+    << completar>>
+    """)
+    
+    # ## 🎯 Objetivo del Trabajo
+    # Analizar tendencias en los trabajos finales de tres carreras universitarias.
+
+    # ## 🛠️ Herramientas utilizadas
+    # - **Python**
+    # - **Pandas**
+    # - **Streamlit**
+    # - **Jupyter Notebooks**
+    # - **Plotly**
+    # - **Procesamiento de Lenguaje Natural**
+    
+    st.markdown("""
+    ## 📁 Organización del Proyecto
+    La interfaz de este sitio está organizada de la siguiente manera:
+    1. Dashboard del dataset principal
+    2. Dashboard del dataset de las tres carreras seleccionadas
+    3. Dashboard de resultados del modelado temático
+    4. Información técnica y metodológica del proyecto
+    """)
+    
